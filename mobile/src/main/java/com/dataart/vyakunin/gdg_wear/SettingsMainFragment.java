d60f73cc97575dd70833bfd6ac6a44b4c547a78e
BukT0p <yak_vick@mail.ru>
2014-10-18 15:37:19 +0300

    Manifest

--- mobile/src/main/java/com/dataart/vyakunin/gdg_wear/SettingsMainFragment.java
+++ mobile/src/main/java/com/dataart/vyakunin/gdg_wear/SettingsMainFragment.java
@@ -1,13 +1,32 @@
 package com.dataart.vyakunin.gdg_wear;
 
 import android.support.v4.app.Fragment;
+import android.widget.Button;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 @EFragment(R.layout.settings_main)
 public class SettingsMainFragment extends Fragment {
-
     public static Fragment newInstance() {
-        return new SettingsMainFragment();
+        return SettingsMainFragment_.builder().build();
     }
+
+    @Click(R.id.settings_message_numbers)
+    void settingsMessageNumbersClicked() {
+
+    }
+
+    @Click(R.id.settings_message_list)
+    void settingsMessageListClicked() {
+
+    }
+
+    @Click(R.id.settings_phone_numbers)
+    void settingsPhoneNumbersClicked() {
+
+    }
+
+
 }
---
+++ mobile/src/main/java/com/dataart/vyakunin/gdg_wear/Store.java
@@ -0,0 +1,35 @@
+package com.dataart.vyakunin.gdg_wear;
+
+import android.provider.BaseColumns;
+
+import com.annotatedsql.annotation.provider.Provider;
+import com.annotatedsql.annotation.provider.URI;
+import com.annotatedsql.annotation.sql.Autoincrement;
+import com.annotatedsql.annotation.sql.Column;
+import com.annotatedsql.annotation.sql.PrimaryKey;
+import com.annotatedsql.annotation.sql.Schema;
+import com.annotatedsql.annotation.sql.Table;
+
+/**
+ * See https://github.com/hamsterksu/Android-AnnotatedSQL
+ */
+@Schema(className = "StoreSchema", dbName = "gdg_wear.db", dbVersion = 24)
+@Provider(authority = "com.dataart.vyakunin.store", name = "StoreContentProvider", schemaClass = "StoreSchema")
+public interface Store {
+
+    @Table(ShortMessages.TABLE_NAME)
+    public static interface ShortMessages {
+        String TABLE_NAME = "short_messages";
+        @URI
+        String CONTENT_URI = "short_messages";
+
+        @PrimaryKey
+        @Autoincrement
+        @Column(type = Column.Type.INTEGER)
+        String ID = BaseColumns._ID;
+
+        @Column(type = Column.Type.TEXT)
+        String VALUE = "value";
+    }
+}
+
