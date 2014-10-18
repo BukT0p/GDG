744b6e1bf6dd227325487b64404a30b2e339e5f9
BukT0p <yak_vick@mail.ru>
2014-10-18 15:42:56 +0300

    actions

--- mobile/src/main/java/com/dataart/vyakunin/gdg_wear/HomeActivity.java
+++ mobile/src/main/java/com/dataart/vyakunin/gdg_wear/HomeActivity.java
@@ -1,22 +1,41 @@
 package com.dataart.vyakunin.gdg_wear;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_home)
 public class HomeActivity extends Activity {
 
+    public static final String SEND_ALERT = "android.gdg.send_alert";
+    public static final String CALL = "android.gdg.call";
+    public static final String RECORD_AUDIO = "android.gdg.record_audio";
+    public static final String RECORD_VIDEO = "android.gdg.record_video";
+
     @Click(R.id.settings_btn)
-    void onSettingsClicked(){
+    void onSettingsClicked() {
         startActivity(SettingsActivity.newIntent(this));
+    }
+
+    @AfterViews
+    void init() {
+        String action = getIntent().getAction();
+        if (action != null) {
+            doAction(action);
+        }
+    }
+
+    private void doAction(String action) {
+        if (action.equals(SEND_ALERT)) {
+            //TODO
+        } else if (action.equals(CALL)) {
+            //TODO
+        } else if (action.equals(RECORD_AUDIO)) {
+            //TODO
+        } else if (action.equals(RECORD_VIDEO)) {
+            //TODO
+        }
     }
 }
